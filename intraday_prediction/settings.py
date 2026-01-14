0b6ad1c8ea22c2d88a89be5fcd356fc366e9b32e
Saksham Jaswal <sakshamjaswal01@gmail.com>
2026-01-14 11:41:53 +0530

    settings changes

--- intraday_prediction/settings.py
+++ intraday_prediction/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['16.171.39.115', 'localhost', '127.0.0.1']
 
 
 # Application definition
