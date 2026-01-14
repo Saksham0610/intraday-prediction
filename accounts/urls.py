04329bf148290cd104fad0f82dd753a386d895c0
Saksham Jaswal <sakshamjaswal01@gmail.com>
2026-01-14 14:29:41 +0530

    login/registration design changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('login/', LoginView.as_view(
+        template_name='accounts/login.html'
+    ), name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
 ]
